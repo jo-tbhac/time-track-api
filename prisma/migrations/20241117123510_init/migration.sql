ffc2d4e2a6a7122bf2935f0b046c817b763a6dfb
junpei ogawa <cornerstone.am3@gmail.com>
2024-11-30 20:50:56 +0900

    change database

--- prisma/migrations/20241117123510_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "jobs" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "hourly_wage" INTEGER NOT NULL,
-
-    CONSTRAINT "jobs_pkey" PRIMARY KEY ("id")
-);
---
+++ prisma/migrations/20241130113740_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "jobs" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "hourly_wage" INTEGER NOT NULL
+);
